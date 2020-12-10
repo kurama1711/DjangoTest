1a79897490b273ad003210bbf890aaae2adf49b9
kurama1711 <nastya.azorkina@gmail.com>
2020-12-10 19:18:35 +0300

    Финальная версия приложения.

--- testDjango/testDjango/settings.py
+++ testDjango/testDjango/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'fpages',
 ]
 
 SITE_ID = 1
@@ -126,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
